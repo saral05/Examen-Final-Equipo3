35fcd31662a2052c40e608e3aaf5aff7fed36050
Tenorio <luisitotenorio19@gmail.com>
2021-12-09 22:36:00 -0600

    creacion de seed, fact, model, migra, control, creacion de login

---
+++ database/factories/MateriaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Materia;
+use Faker\Generator as Faker;
+
+$factory->define(Materia::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/SalonesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Salones;
+use Faker\Generator as Faker;
+
+$factory->define(Salones::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
